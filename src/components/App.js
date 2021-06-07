871528f327fef6b0ab8c7174499c7f007904b318
Thinh Nguyen <thinhpnguyen97@gmail.com>
2021-06-06 23:29:42 -0700

    quick fix for highlighting min

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,18 @@
-import React from 'react';
+import React, {useState} from 'react';
 import ReactDOM from 'react-dom';
 import Graph from './graph.js';
 
 function App() {
+  // const [sortEn, updateSortEn] = useState(false);
+  // const [chartCtx, updateChartCtx] = useState();
+
+  // function sortHandler(){
+  //   updateSortEn(true);
+  // };
+
   return (
     <div >
-      <Graph />
+      <Graph  />
     </div>
   );
 }
--- src/components/graph.js
+++ src/components/graph.js
@@ -1,12 +1,10 @@
 import React, { useState, useEffect } from 'react';
 import Chart from 'chart.js/auto';
 
-function Graph(){
-    // const [nums, setNums] = useState([12, 11, 3, 5, 2, 4, 8, 9, 10]);
-    // const [xAxis, setXAxis] = useState(['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange', 'lol', 'lmao' , 'ok']);
-    let nums = [12, 11, 3, 5, 2, 4, 33, 21, 13, 7, 10];
-    let xAxis = ['12', '11', '3', '5', '2', '4', '33', '21', '13', '7', '10'];
+function Graph(props){
     useEffect(()=>{
+        let nums = [12, 11, 3, 5, 2, 4, 33, 21, 13, 7, 10];
+        let xAxis = ['12', '11', '3', '5', '2', '4', '33', '21', '13', '7', '10'];
         var ctx = document.getElementById('myChart');
         var myChart = new Chart(ctx, {
             type: 'bar',
@@ -45,84 +43,83 @@ function Graph(){
                 }
             }
         });
+        let inputArr = myChart.data.datasets[0].data;
+        let labels = myChart.data.labels;
+        let colors = myChart.data.datasets[0].backgroundColor;
+        //let borderColors = myChart.data.datasets[0].borderColor;
+        
+        let n = inputArr.length;
 
-    let inputArr = myChart.data.datasets[0].data;
-    let labels = myChart.data.labels;
-    let colors = myChart.data.datasets[0].backgroundColor;
-    //let borderColors = myChart.data.datasets[0].borderColor;
-    
-    let n = inputArr.length;
-
-    //used to slow down a loop
-    const sleep = (milliseconds) => {
-        return new Promise(resolve => setTimeout(resolve, milliseconds))
-      }
-
-    function highlight(i){
-        colors[i] =  'rgba(255, 99, 132, 1)';
-        myChart.update();
-    }
-
-    function unhighlight(i){
-        colors[i] =  'rgba(255, 99, 132, 0.2)';
-        myChart.update();
-    }
-
-    function highlightMin(i){
-        colors[i] =  'rgba(54, 162, 235, 0.2)';
-        myChart.update();
-    }
-
-    const sort = async () =>{
-        for(let i = 0; i < n; i++) {
-            //change color if ith column
-            highlight(i);
-
-            // Finding the smallest number in the subarray
-            let min = i;
-            for(let j = i+1; j < n; j++){
-                highlight(j);
-                if(inputArr[j] < inputArr[min]) {
-                    if (min !== i){
-                        unhighlight(min);
-                    }
-                    min=j;
-                    highlightMin(min);
-                    await sleep(600);
-                    continue;
-
-                }
-                await sleep(600);
-                unhighlight(j);
-             }
-             if (min !== i) {
-                 unhighlight(min);
-                 // Swapping the elements
-                 let tmp = inputArr[i]; 
-                 let labelTmp = labels[i];
-                 //let colorTmp = colors[i];
-                 //let borderColorTmp = borderColors[i];
-
-                 inputArr[i] = inputArr[min];
-                 labels[i] = labels[min];
-                 //colors[i] = colors[min];
-                // borderColors[i] = borderColors[min];
-
-                 inputArr[min] = tmp;
-                 labels[min] = labelTmp;
-                // colors[min] = colorTmp;
-                // borderColors[min] = borderColorTmp;
-
-                 myChart.update();  
-            }
-
-            //change color back
-            unhighlight(i);
-           
+        //used to slow down a loop
+        const sleep = (milliseconds) => {
+            return new Promise(resolve => setTimeout(resolve, milliseconds))
         }
-    }
 
-    sort();
+        function highlight(i){
+            colors[i] =  'rgba(255, 99, 132, 1)';
+            myChart.update();
+        }
+
+        function unhighlight(i){
+            colors[i] =  'rgba(255, 99, 132, 0.2)';
+            myChart.update();
+        }
+
+        function highlightMin(i){
+            colors[i] =  'rgba(54, 162, 235, 0.2)';
+            myChart.update();
+        }
+
+        const sort = async () =>{
+            for(let i = 0; i < n; i++) {
+                //change color if ith column
+                highlight(i);
+
+                // Finding the smallest number in the subarray
+                let min = i;
+                let exMin = i;
+                for(let j = i+1; j < n; j++){
+                    //first hightlight the pointer
+                    highlight(j);
+                    if(inputArr[j] < inputArr[min]) {
+                        exMin = min;
+                        min=j;
+
+                    }
+                    await sleep(50);
+                    unhighlight(j);
+
+                    //change the hightlight if min changes
+                    if (min !== exMin){
+                        if(exMin !== i){
+                            unhighlight(exMin);
+                        }
+                        highlightMin(min);
+                    }
+                }
+                if (min !== i) {
+                    unhighlight(min);
+                    // Swapping the elements and labels
+                    let tmp = inputArr[i]; 
+                    let labelTmp = labels[i];
+                
+
+                    inputArr[i] = inputArr[min];
+                    labels[i] = labels[min];
+
+                    inputArr[min] = tmp;
+                    labels[min] = labelTmp;
+
+                    myChart.update();  
+                }
+
+                //change color back
+                unhighlight(i);
+            
+            }
+        }
+        sort();
+    
     
         
     });
